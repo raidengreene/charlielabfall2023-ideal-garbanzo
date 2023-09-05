fa8515f211b6139052c8407f1986e7e1f9815aa4
raidengreene <142539260+raidengreene@users.noreply.github.com>
2023-09-05 14:25:48 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // put code in this place
+  var bcounter = 0;
+  document.getElementById("bcount").innerHTML = bcounter;
+  $("#upbonk").click(
+  function(
+  ){
+    // code here
+    $("#upbonk").toggle();
+    $("#downbonk").toggle();
+    bcounter += 1
+    document.getElementById("bcount").innerHTML = bcounter;
+    var audio = new Audio("../assests/bonk-sound-effect.mp3");
+    audio.play();
+  })
+  $("#downbonk").click(
+  function(
+  ){
+  // code here
+    $("#upbonk").toggle();
+    $("#downbonk").toggle();
+  })
+});
